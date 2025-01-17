a6843b77ac2b88a1ddd55bbca96695ca8ad86b09
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-18 01:32:30 +0300

    added LocationDTO.java

--- src/main/java/com/example/pogodaspring/weather/dto/LocationDTO.java
+++ src/main/java/com/example/pogodaspring/weather/dto/LocationDTO.java
@@ -1,15 +1,19 @@
 package com.example.pogodaspring.weather.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-@Builder
-public record LocationDTO(
-        int id,
-        String name,
-        int userId,
-        BigDecimal lat,
-        BigDecimal lon,
-        WeatherResponseDTO weatherResponseDTO) {
 
+@Setter
+@Getter
+@Builder
+public class LocationDTO {
+    private int id;
+    private String name;
+    private int userId;
+    private BigDecimal lat;
+    private BigDecimal lon;
+    private WeatherResponseDTO weatherResponseDTO;
 }
