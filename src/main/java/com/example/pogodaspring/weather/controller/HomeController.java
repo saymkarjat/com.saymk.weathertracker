70e56d9441506129743901ba79c9b085ae729e49
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-17 22:33:51 +0300

    added annotation @RequestParam instead of HttpServlet request

--- src/main/java/com/example/pogodaspring/weather/controller/HomeController.java
+++ src/main/java/com/example/pogodaspring/weather/controller/HomeController.java
@@ -4,16 +4,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/app")
 public class HomeController {
 
     @GetMapping("home")
-    public String home(HttpServletRequest request, Model model) {
-        String username = (String) request.getAttribute("username");
-        Boolean isUserAuthenticated = (Boolean) request.getAttribute("isUserAuthenticated");
+    public String home(@RequestAttribute(name = "username", required = false) String username,
+                       @RequestAttribute(name = "isUserAuthenticated") Boolean isUserAuthenticated,
+                       Model model) {
         model.addAttribute("username", username);
         model.addAttribute("isUserAuthenticated", isUserAuthenticated);
         return "home";
