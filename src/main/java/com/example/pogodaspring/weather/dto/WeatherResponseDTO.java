0acb9ccd82e61a35f764421b5e62b21fbe378ab7
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-20 01:46:22 +0300

    added rounding for fields

--- src/main/java/com/example/pogodaspring/weather/dto/WeatherResponseDTO.java
+++ src/main/java/com/example/pogodaspring/weather/dto/WeatherResponseDTO.java
@@ -2,6 +2,7 @@ package com.example.pogodaspring.weather.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,8 @@ public class WeatherResponseDTO {
 
     @JsonProperty("weather")
     private Weather[] weather;
+    @JsonProperty("sys")
+    private Sys sys;
 
     @Getter
     @Setter
@@ -24,13 +27,23 @@ public class WeatherResponseDTO {
     public static class WeatherInfo {
         // Геттеры
         @JsonProperty("temp")
-        private double temp;
+        private int temp;
 
         @JsonProperty("feels_like")
-        private double feelsLike;
+        private int feelsLike;
 
         @JsonProperty("humidity")
         private int humidity;
+
+        @JsonSetter("temp")
+        public void setTemp(double temp) {
+            this.temp = (int) Math.round(temp);
+        }
+
+        @JsonSetter("feels_like")
+        public void setFeelsLike(double feelsLike) {
+            this.feelsLike = (int) Math.round(feelsLike);
+        }
 
     }
 
@@ -44,6 +57,16 @@ public class WeatherResponseDTO {
 
         @JsonProperty("description")
         private String description;
+
+    }
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Sys {
+
+        @JsonProperty("country")
+        private String country;
+
 
     }
 }
