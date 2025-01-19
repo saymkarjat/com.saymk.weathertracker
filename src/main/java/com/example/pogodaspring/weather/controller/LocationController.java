2f16aa5d777a3c52d9b7a68d0a89091e0df8ac96
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-20 01:45:59 +0300

    added LocationController for adding and deleting locations

--- src/main/java/com/example/pogodaspring/weather/controller/LocationController.java
+++ src/main/java/com/example/pogodaspring/weather/controller/LocationController.java
@@ -1,20 +1,43 @@
 package com.example.pogodaspring.weather.controller;
 
 
+import com.example.pogodaspring.weather.service.LocationService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/app/location")
+@AllArgsConstructor
 public class LocationController {
     private final int limit = 5;
+    private LocationService locationService;
 
-    @PostMapping("search")
-    public String home(@RequestParam String locationName, Model model) {
+    @PostMapping("add")
+    public String add(@RequestAttribute(name = "username") String username,
+                      @RequestAttribute(name = "isUserAuthenticated") Boolean isUserAuthenticated,
+                          @RequestParam(name = "latitude") String latitude,
+                      @RequestParam(name = "longitude") String longitude,
+                      @RequestParam(name = "city") String city) {
+        if (!isUserAuthenticated){
+            return "redirect:/auth/logout";
+        }
+        locationService.addNewLocation(latitude, longitude, city, username);
+        return "redirect:/app/home";
+    }
 
-        return "home";
+    @PostMapping("delete")
+    public String delete(@RequestAttribute(name = "username") String username,
+                      @RequestAttribute(name = "isUserAuthenticated") Boolean isUserAuthenticated,
+                      @RequestParam(name = "locationId") String locationId) {
+        if (!isUserAuthenticated){
+            return "redirect:/auth/logout";
+        }
+        locationService.deleteLocationById(Integer.parseInt(locationId), username);
+        return "redirect:/app/home";
     }
 }
