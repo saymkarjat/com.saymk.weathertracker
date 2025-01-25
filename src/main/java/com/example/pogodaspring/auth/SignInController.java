2d26803f32b28c1232d180cf5f5ccb2fffdfe0e4
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2025-01-25 13:48:20 +0300

    fix cookie.setSecure(true)

--- src/main/java/com/example/pogodaspring/auth/SignInController.java
+++ src/main/java/com/example/pogodaspring/auth/SignInController.java
@@ -52,7 +52,7 @@ public class SignInController {
         SessionDTO sessionDTO = signInService.authenticate(userDTO);
         Cookie cookie = new Cookie("session_id", sessionDTO.id().toString());
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(2 * 3600);
         resp.addCookie(cookie);
